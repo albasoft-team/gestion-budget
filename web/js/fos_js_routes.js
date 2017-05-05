3167bec9b637aa80a08ce86a070ef33a2b128912
albasoft-team <ibrahima-sory.diallo@albasoft.fr>
2017-05-05 19:57:00 +0000

    utilisation fosjsroutingbundle

---
+++ web/js/fos_js_routes.js
@@ -0,0 +1,1 @@
+fos.Router.setData({"base_url":"","routes":{"all_donneesbudget":{"tokens":[["text","\/donneesbudget\/allDonneesBudget"]],"defaults":[],"requirements":[],"hosttokens":[]},"edit_donneesbudget":{"tokens":[["text","\/donneesbudget\/editDonneeBudget"]],"defaults":[],"requirements":[],"hosttokens":[]}},"prefix":"","host":"localhost","scheme":"http"});
